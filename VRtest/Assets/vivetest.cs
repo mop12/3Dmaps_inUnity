830c74d6cac209458e6782dd0614e82a805ed8ac
scrim_vs <bgtm1260133@gn.iwasaki.ac.jp>
2017-06-02 11:31:56 +0900

    first

---
+++ VRtest/Assets/vivetest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vivetest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SteamVR_Controller controller;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
